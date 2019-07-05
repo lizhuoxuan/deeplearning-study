624959fa5581dafbfdce93084887ca66d0184674
lzx <123456789>
2019-07-05 09:21:47 +0800

    1

---
+++ img_test.py
@@ -0,0 +1,7 @@
+from matplotlib.image import imread
+import matplotlib.pyplot as plt
+
+img = imread("d:/1.jpg")
+plt.imshow(img)
+
+plt.show()
---
+++ np_test.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# x = np.array([[1, 2], [3, 4]])
+# y = np.array([[5, 6], [7, 8]])
+# print(x * y)
+# print(x[x > 1])
+# x = x.flatten()
+# print(np.array([0, 2]))
+# print(x[np.array([0, 2])])
+# print(np.arange(0, 6, 0.1))
+x = np.arange(0, 6, 0.1)
+y1 = np.sin(x)
+y2 = np.cos(x)
+# print(y)
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, linestyle="--", label="cos")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title('sin & y')
+plt.legend()
+plt.show()
